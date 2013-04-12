sebhtml/RayPlatform

commit 310770c366fe34ae72e44913bbfef650dbe8d72a

improve the patch for message routing with a configuration
Resolves-bug: https://github.com/sebhtml/ray/issues/170
Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

core/ComputeCore.cpp
@@ -136,6 +136,8 @@ void ComputeCore::run(){
 	// problems during configuration
 	m_routerIsEnabled=m_router.isEnabled();
 
+	configureEngine();
+
 	if(!m_resolvedSymbols)
 		resolveSymbols();
 
@@ -774,8 +776,6 @@ void ComputeCore::constructor(int argc,char**argv,int miniRankNumber,int numberO
 
 	m_doChecksum=false;
 
-	m_addExtraBytes = true;
-
 	m_miniRanksAreEnabled=useMiniRanks;
 
 	#ifdef CONFIG_DEBUG_CORE
@@ -809,13 +809,17 @@ void ComputeCore::constructor(int argc,char**argv,int miniRankNumber,int numberO
 	m_rank=miniRankNumber;
 	m_size=numberOfMiniRanks;
 
+}
+
+void ComputeCore::configureEngine() {
+
 	// set the number of buffers to use
 	int minimumNumberOfBuffers=128;
 
 	int availableBuffers=minimumNumberOfBuffers;
 
 	// even a message with a NULL buffer requires a buffer for routing
-	if(m_routerIsEnabled||useMiniRanks)
+	if(m_routerIsEnabled|| m_miniRanksAreEnabled)
 		availableBuffers=m_size*2;
 
 	// this will occur when using the virtual router with a few processes
@@ -861,8 +865,7 @@ void ComputeCore::constructor(int argc,char**argv,int miniRankNumber,int numberO
 
 	// add a message unit to store the checksum or the routing information
 	// with 64-bit integers as MessageUnit, this is 4008 bytes or 501 MessageUnit maximum
-	// TODO: RayPlatform can not do both routing and checksums 
-	if(useMiniRanks || m_doChecksum || m_routerIsEnabled || m_addExtraBytes){
+	if(m_miniRanksAreEnabled || m_doChecksum || m_routerIsEnabled){
 		if(sizeof(MessageUnit)>=4){
 			maximumMessageSizeInByte+=2*sizeof(MessageUnit);
 		}else if(sizeof(MessageUnit)>=2){

core/ComputeCore.h
@@ -75,8 +75,6 @@ class ComputeCore{
  */
 	MessagesHandler*m_messagesHandler;
 
-	bool m_addExtraBytes;
-
 /*
  * In the legacy mode, RayPlatform will not
  * spawn a thread for MPI and every MPI rank will do
@@ -236,6 +234,7 @@ class ComputeCore{
 
 	void registerDummyPlugin();
 	bool isPublicMasterMode(PluginHandle plugin,MasterMode mode);
+	void configureEngine();
 public:
 	/** this is the main method */
 	void run();

commit cda5eb537966098ec1ce31b6942a70a13a613d45

core: fix a regression for registered handle names
The regression was pushed in 310770c366fe34ae72e44913bbfef650dbe8d72a.

Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

core/ComputeCore.cpp
@@ -809,6 +809,19 @@ void ComputeCore::constructor(int argc,char**argv,int miniRankNumber,int numberO
 	m_rank=miniRankNumber;
 	m_size=numberOfMiniRanks;
 
+	for(int i=0;i<MAXIMUM_NUMBER_OF_MASTER_HANDLERS;i++){
+		strcpy(MASTER_MODES[i],"UnnamedMasterMode");
+	}
+	for(int i=0;i<MAXIMUM_NUMBER_OF_SLAVE_HANDLERS;i++){
+		strcpy(SLAVE_MODES[i],"UnnamedSlaveMode");
+	}
+	for(int i=0;i<MAXIMUM_NUMBER_OF_TAG_HANDLERS;i++){
+		strcpy(MESSAGE_TAGS[i],"UnnamedMessageTag");
+	}
+
+	m_currentSlaveModeToAllocate=0;
+	m_currentMasterModeToAllocate=0;
+	m_currentMessageTagToAllocate=0;
 }
 
 void ComputeCore::configureEngine() {
@@ -903,20 +916,6 @@ void ComputeCore::configureEngine() {
 	cout<<"[ComputeCore] allocated "<<m_maximumAllocatedInboxBuffers<<" buffers of size "<<maximumMessageSizeInByte<<" for inbox messages"<<endl;
 	cout<<"[ComputeCore] allocated "<<m_maximumAllocatedOutboxBuffers<<" buffers of size "<<maximumMessageSizeInByte<<" for outbox messages"<<endl;
 	#endif
-
-	for(int i=0;i<MAXIMUM_NUMBER_OF_MASTER_HANDLERS;i++){
-		strcpy(MASTER_MODES[i],"UnnamedMasterMode");
-	}
-	for(int i=0;i<MAXIMUM_NUMBER_OF_SLAVE_HANDLERS;i++){
-		strcpy(SLAVE_MODES[i],"UnnamedSlaveMode");
-	}
-	for(int i=0;i<MAXIMUM_NUMBER_OF_TAG_HANDLERS;i++){
-		strcpy(MESSAGE_TAGS[i],"UnnamedMessageTag");
-	}
-
-	m_currentSlaveModeToAllocate=0;
-	m_currentMasterModeToAllocate=0;
-	m_currentMessageTagToAllocate=0;
 
 }
 