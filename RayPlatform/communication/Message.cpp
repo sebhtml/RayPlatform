sebhtml/RayPlatform

commit a01f97eae41bcd759bfc521d84053552cf38d521

fix the code for testing message integrity
Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

RayPlatform/core/ComputeCore.cpp
@@ -845,24 +845,26 @@ void ComputeCore::sendMessages(){
 void ComputeCore::addMessageChecksums(){
 
 	int count=m_outbox.size();
-	for(int i=0;i<count;i++){
-		MessageUnit*data=m_outbox.at(i)->getBuffer();
-		int numberOfUnits=m_outbox.at(i)->getCount();
 
+	for(int i=0;i<count;i++){
+
+		char * data=m_outbox.at(i)->getBufferBytes();
+		int numberOfBytes = m_outbox.at(i)->getNumberOfBytes();
+
+		/*
 		// don't compute checksum for empty messages
-		if(numberOfUnits==0){
+		if(bytes == 0){
 			continue;
 		}
-
-		int numberOfBytes=sizeof(MessageUnit)*numberOfUnits;
+		*/
 
 		uint8_t*bytes=(uint8_t*)data;
 
 		uint32_t crc32=computeCyclicRedundancyCode32(bytes,numberOfBytes);
 
-		data[numberOfUnits]=crc32;
+		memcpy(bytes + numberOfBytes, &crc32, sizeof(crc32));
 
-		m_outbox.at(i)->setCount(numberOfUnits+1);
+		m_outbox.at(i)->setNumberOfBytes(numberOfBytes + sizeof(crc32));
 	}
 
 }
@@ -871,28 +873,37 @@ void ComputeCore::verifyMessageChecksums(){
 
 	int count=m_inbox.size();
 	for(int i=0;i<count;i++){
-		MessageUnit*data=m_inbox.at(i)->getBuffer();
 
-		// this contains the checksum unit too.
-		int numberOfUnits=m_inbox.at(i)->getCount();
+		Message * message = m_inbox.at(i);
 
-		// don't compute checksum for empty messages
-		if(numberOfUnits==0){
-			continue;
-		}
+		uint32_t crc32 = 0;
+		char * data = message->getBufferBytes();
+
+#ifdef CONFIG_ASSERT
+		assert(message->getNumberOfBytes() >= (int) sizeof(crc32));
+#endif
+
+		message->setNumberOfBytes(message->getNumberOfBytes() - sizeof(crc32));
+
+#ifdef CONFIG_ASSERT
+		assert(message->getNumberOfBytes() >= 0);
+#endif
 
 		// the last MessageUnit contains the crc32.
 		// note that the CRC32 feature only works for MessageUnit at least
 		// 32 bits.
-		int numberOfBytes=sizeof(MessageUnit)*(numberOfUnits-1);
+		int numberOfBytes = m_inbox.at(i)->getNumberOfBytes();
 
 		uint8_t*bytes=(uint8_t*)data;
 
 		// compute the checksum, excluding the reference checksum
 		// from the data
-		uint32_t crc32=computeCyclicRedundancyCode32(bytes,numberOfBytes);
+		crc32 = computeCyclicRedundancyCode32((uint8_t*)bytes, numberOfBytes);
 
-		uint32_t expectedChecksum=data[numberOfUnits-1];
+		uint32_t expectedChecksum = 0;
+
+		memcpy(&expectedChecksum, data + numberOfBytes,
+			sizeof(expectedChecksum));
 
 		#ifdef CONFIG_SHOW_CHECKSUM
 		cout<<" Expected checksum (CRC32): "<<hex<<expectedChecksum<<endl;
@@ -902,25 +913,14 @@ void ComputeCore::verifyMessageChecksums(){
 		if(crc32 != expectedChecksum){
 			Message*message=m_inbox.at(i);
 			cout<<"Error: RayPlatform detected a message corruption !"<<endl;
-			cout<<" Tag: "<<MESSAGE_TAGS[message->getTag()]<<endl;
+			cout<<" Tag: " << message->getTag() << endl;
 			cout<<" Source: "<<message->getSource()<<endl;
 			cout<<" Destination: "<<message->getDestination()<<endl;
-			cout<<" sizeof(MessageUnit): "<<sizeof(MessageUnit)<<endl;
-			cout<<" Count (excluding checksum): "<<numberOfUnits-1<<endl;
+			cout<<" Bytes (excluding checksum): "<< numberOfBytes << endl;
 			cout<<" Expected checksum (CRC32): "<<hex<<expectedChecksum<<endl;
 			cout<<" Actual checksum (CRC32): "<<hex<<crc32<<dec<<endl;
 		}
 
-		#ifdef ASSERT
-		assert(numberOfUnits>=2);
-		#endif
-
-		// remove the checksum
-		m_inbox.at(i)->setCount(numberOfUnits-1);
-
-		#ifdef ASSERT
-		assert(numberOfUnits>=1);
-		#endif
 	}
 
 }
@@ -956,14 +956,17 @@ void ComputeCore::receiveMessages(){
 	//cout << "DEBUG loading message metadata." << endl;
 
 	Message * importantMessage = m_inbox.at(0);
-	importantMessage->loadRoutingMetaData();
-	importantMessage->loadActorMetaData();
-
 
 	// verify checksums and remove them
+	// messages are line onions, you have to peel them from the outside to the inside
 	if(m_doChecksum){
 		verifyMessageChecksums();
 	}
+
+
+	importantMessage->loadRoutingMetaData();
+	importantMessage->loadActorMetaData();
+
 
 	for(int i=0;i<(int)m_inbox.size();i++){
 		m_tickLogger.logReceivedMessage(INVALID_HANDLE);
@@ -1187,14 +1190,18 @@ void ComputeCore::configureEngine() {
 	// add a message unit to store the checksum or the routing information
 	// with 64-bit integers as MessageUnit, this is 4008 bytes or 501 MessageUnit maximum
 	// also add 8 bytes for actor metadata
+	// finally, add 4 bytes for the checksum, if necessary.
+
 	if(m_miniRanksAreEnabled || m_doChecksum || m_routerIsEnabled || useActorModel){
-		if(sizeof(MessageUnit)>=4){
-			maximumMessageSizeInByte+=(2 + 2)*sizeof(MessageUnit);
-		}else if(sizeof(MessageUnit)>=2){
-			maximumMessageSizeInByte+=(2 + 2) *2*sizeof(MessageUnit);
-		}else{
-			maximumMessageSizeInByte+=(2 + 2) *4*sizeof(MessageUnit);
-		}
+
+		// actor model
+		maximumMessageSizeInByte += 2 * sizeof(int) * sizeof(char);
+
+		// routing
+		maximumMessageSizeInByte += 2 * sizeof(int) * sizeof(char);
+
+		// checksum
+		maximumMessageSizeInByte += 1 * sizeof(uint32_t) * sizeof(char);
 	}
 
 	m_inboxAllocator.constructor(m_maximumAllocatedInboxBuffers,

commit b862e4a9956876f1edd246de757f7cd10dd47281

fix a regression introduced in a01f97eae41bcd759bfc521d84053552cf38d521
Internal buffers must be aligned on 8 bytes because a lot of framework
code assumes that this alignment is there.

Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

RayPlatform/communication/Message.cpp
@@ -43,12 +43,6 @@ using namespace std;
  *
 
  */
-#define MESSAGE_META_DATA_ACTOR_SOURCE		(0 * sizeof(int) )
-#define MESSAGE_META_DATA_ACTOR_DESTINATION	(1 * sizeof(int) )
-#define MESSAGE_META_DATA_ROUTE_SOURCE	 	(2 * sizeof(int) )
-#define MESSAGE_META_DATA_ROUTE_DESTINATION	(3 * sizeof(int) )
-#define MESSAGE_META_DATA_CHECKSUM	 	(4 * sizeof(int) )
-
 
 Message::Message() {
 
@@ -72,8 +66,10 @@ void Message::initialize() {
 Message::~Message() {
 }
 
-/** buffer must be allocated or else it will CORE DUMP. */
-Message::Message(MessageUnit*b,int c,Rank dest,MessageTag tag,Rank source){
+/**
+ * buffer must be allocated or else it will CORE DUMP.
+ */
+Message::Message(void*b,int c,Rank dest,MessageTag tag,Rank source){
 
 	initialize();
 

RayPlatform/communication/Message.h
@@ -1,48 +1,57 @@
 /*
- 	RayPlatform: a message-passing development framework
-    Copyright (C) 2010, 2011, 2012 Sébastien Boisvert
+RayPlatform: a message-passing development framework
+Copyright (C) 2010, 2011, 2012 Sébastien Boisvert
 
-	http://github.com/sebhtml/RayPlatform
+http://github.com/sebhtml/RayPlatform
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, version 3 of the License.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, version 3 of the License.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
 
-    You have received a copy of the GNU Lesser General Public License
-    along with this program (lgpl-3.0.txt).
-	see <http://www.gnu.org/licenses/>
+You have received a copy of the GNU Lesser General Public License
+along with this program (lgpl-3.0.txt).
+see <http://www.gnu.org/licenses/>
 
 */
 
 #ifndef _Message_H
 #define _Message_H
 
+#define MESSAGE_META_DATA_ACTOR_SOURCE		0
+#define MESSAGE_META_DATA_ACTOR_DESTINATION	( MESSAGE_META_DATA_ACTOR_SOURCE + sizeof(int) )
+#define MESSAGE_META_DATA_ROUTE_SOURCE	 	( MESSAGE_META_DATA_ACTOR_DESTINATION + sizeof(int) )
+#define MESSAGE_META_DATA_ROUTE_DESTINATION	( MESSAGE_META_DATA_ROUTE_SOURCE + sizeof(int))
+#define MESSAGE_META_DATA_CHECKSUM	 	( MESSAGE_META_DATA_ROUTE_DESTINATION + sizeof(int))
+#define MESSAGE_META_DATA_SIZE                  ( MESSAGE_META_DATA_CHECKSUM + sizeof(uint32_t) )
+
+
+
 #include <RayPlatform/core/types.h>
 
 #include <stdint.h>
 
 /**
- * In Ray, every message is a Message.
- * the inbox and the outbox are arrays of Message's
- * All the code in Ray utilise Message to communicate information.
- * MPI_Datatype is always MPI_UNSIGNED_LONG_LONG
- * m_count is >=0 and <= MAXIMUM_MESSAGE_SIZE_IN_BYTES/sizeof(uint64_t)
- *  (default is 4000/8 = 500 ).
- *
- * in the buffer:
- *
- *  Application payload (works)
- *  int source actor (works)
- *  int destination actor (works)
- *  int routing source (works)
- *  int routing destination (works)
- *  int checksum (probably broken)
- *
+* In Ray, every message is a Message.
+* the inbox and the outbox are arrays of Message's
+* All the code in Ray utilise Message to communicate information.
+* MPI_Datatype is always MPI_UNSIGNED_LONG_LONG
+* m_count is >=0 and <= MAXIMUM_MESSAGE_SIZE_IN_BYTES/sizeof(uint64_t)
+*  (default is 4000/8 = 500 ).
+*
+* in the buffer:
+*
+*  Application payload (works)
+*  int source actor (works)
+*  int destination actor (works)
+*  int routing source (works)
+*  int routing destination (works)
+*  int checksum (probably broken)
+*
  * The offset (see Message.cpp) are relative to the number of bytes *BEFORE*
  * adding this metadata.
  * Note that the m_count is the number of MessageUnit for the message
@@ -116,7 +125,14 @@ private:
 public:
 	Message();
 	~Message();
-	Message(MessageUnit * b,int c,Rank dest,MessageTag tag,Rank source);
+
+	/**
+	 * numberOf8Bytes is there for historical reasons.
+	 * You can call setNumberOfBytes afterward to set the number of bytes anyway.
+	 *
+	 */
+	Message(void* buffer, int numberOf8Bytes, Rank destination, MessageTag tag,
+			Rank source);
 	MessageUnit *getBuffer();
 	char * getBufferBytes();
 	int getCount() const;

RayPlatform/communication/MessagesHandler.cpp
@@ -656,7 +656,7 @@ void MessagesHandler::probeAndRead(Rank source,MessageTag tag,
 	assert(count >= 0);
 	#endif
 
-	MessageUnit staticBuffer[MAXIMUM_MESSAGE_SIZE_IN_BYTES/sizeof(MessageUnit)+2];
+	char staticBuffer[MAXIMUM_MESSAGE_SIZE_IN_BYTES + MESSAGE_META_DATA_SIZE];
 
 	MPI_Recv(staticBuffer,count,datatype,actualSource,actualTag,MPI_COMM_WORLD,&status);
 
@@ -1124,9 +1124,9 @@ void MessagesHandler::receiveMessages(StaticVector*inbox,RingAllocator*inboxAllo
 	assert(count >= 0);
 	#endif
 
-	MessageUnit*incoming=NULL;
+	char * incoming=NULL;
 	if(count > 0){
-		incoming=(MessageUnit*)inboxAllocator->allocate(count * sizeof(char));
+		incoming=(char*)inboxAllocator->allocate(count * sizeof(char));
 	}
 
 	MPI_Recv(incoming,count,datatype,actualSource,actualTag,MPI_COMM_WORLD,&status);

RayPlatform/core/ComputeCore.cpp
@@ -671,7 +671,7 @@ void ComputeCore::sendMessages(){
 			assert(forceMemoryReallocation || aMessage->getCount()==0||numberOfMessages==m_size);
 			#endif
 
-			char * buffer=(char*)(MessageUnit*)m_outboxAllocator.allocate(MAXIMUM_MESSAGE_SIZE_IN_BYTES);
+			char * buffer=(char*)m_outboxAllocator.allocate(MAXIMUM_MESSAGE_SIZE_IN_BYTES);
 
 			#ifdef ASSERT
 			assert(buffer!=NULL);
@@ -739,7 +739,7 @@ void ComputeCore::sendMessages(){
 		// we need a buffer to store the actor metadata
 		if(message->getBuffer() == NULL) {
 			void * buffer = m_outboxAllocator.allocate(0);
-			message->setBuffer((MessageUnit*)buffer);
+			message->setBuffer(buffer);
 		}
 
 		/*
@@ -889,8 +889,8 @@ void ComputeCore::verifyMessageChecksums(){
 		assert(message->getNumberOfBytes() >= 0);
 #endif
 
-		// the last MessageUnit contains the crc32.
-		// note that the CRC32 feature only works for MessageUnit at least
+		// the last Message Unit contains the crc32.
+		// note that the CRC32 feature only works for Message Unit at least
 		// 32 bits.
 		int numberOfBytes = m_inbox.at(i)->getNumberOfBytes();
 
@@ -1056,7 +1056,7 @@ void ComputeCore::constructor(int argc,char**argv,int miniRankNumber,int numberO
 	#endif
 
 	// checksum calculation is only tested
-	// for cases with sizeof(MessageUnit)>=4 bytes
+	// for cases with sizeof(Message Unit)>=4 bytes
 
 	m_routerIsEnabled=false;
 
@@ -1183,33 +1183,75 @@ void ComputeCore::configureEngine() {
 	cout<<"[ComputeCore] the outbox capacity is "<<m_maximumNumberOfOutboxMessages<<" message"<<endl;
 	#endif
 
-	int maximumMessageSizeInByte=MAXIMUM_MESSAGE_SIZE_IN_BYTES;
+	int maximumMessageSizeInBytes = MAXIMUM_MESSAGE_SIZE_IN_BYTES;
 
 	bool useActorModel = true;
 
 	// add a message unit to store the checksum or the routing information
-	// with 64-bit integers as MessageUnit, this is 4008 bytes or 501 MessageUnit maximum
+	// with 64-bit integers as Message Unit, this is 4008 bytes or 501 Message Unit maximum
 	// also add 8 bytes for actor metadata
 	// finally, add 4 bytes for the checksum, if necessary.
 
 	if(m_miniRanksAreEnabled || m_doChecksum || m_routerIsEnabled || useActorModel){
 
+		int headerSize = 0;
+
+		headerSize = MESSAGE_META_DATA_SIZE;
+
+		/*
 		// actor model
-		maximumMessageSizeInByte += 2 * sizeof(int) * sizeof(char);
+		headerSize += ( 2 * sizeof(int) * sizeof(char) );
 
 		// routing
-		maximumMessageSizeInByte += 2 * sizeof(int) * sizeof(char);
+		headerSize += ( 2 * sizeof(int) * sizeof(char));
 
 		// checksum
-		maximumMessageSizeInByte += 1 * sizeof(uint32_t) * sizeof(char);
+		headerSize += ( 1 * sizeof(uint32_t) * sizeof(char) );
+*/
+
+		cout << "DEBUG MAXIMUM_MESSAGE_SIZE_IN_BYTES " << MAXIMUM_MESSAGE_SIZE_IN_BYTES;
+		cout << " headerSize " << headerSize << endl;
+
+		// align this on 8 bytes because there is still something in the code
+		// that is incorrect apparently.
+		//
+		// TODO: find why this needs to be aligned on 8 bytes. This is related to 
+		// sizeof(MessageUnit) being 8 bytes.
+		//
+		// => it is likely because of the underlying system that evolved to
+		// use 64 bits per message unit.
+
+		bool align = true;
+		//align = false;
+
+		int base = 8;
+		int remainder = headerSize % base;
+
+		if(align && remainder != 0) {
+	
+			int toAdd = base - remainder;
+
+			headerSize += toAdd;
+		}
+
+		cout << " headerSize (with padding) " << headerSize << endl;
+
+		maximumMessageSizeInBytes += headerSize;
 	}
 
+	cout << "DEBUG maximumMessageSizeInBytes " << maximumMessageSizeInBytes << endl;
+
+#ifdef CONFIG_ASSERT
+	assert(maximumMessageSizeInBytes >= MAXIMUM_MESSAGE_SIZE_IN_BYTES);
+	assert(maximumMessageSizeInBytes >= MAXIMUM_MESSAGE_SIZE_IN_BYTES + 20);// we need 20 bytes for storing message header
+#endif
+
 	m_inboxAllocator.constructor(m_maximumAllocatedInboxBuffers,
-		maximumMessageSizeInByte,
+		maximumMessageSizeInBytes,
 		"RAY_MALLOC_TYPE_INBOX_ALLOCATOR",false);
 
 	m_outboxAllocator.constructor(m_maximumAllocatedOutboxBuffers,
-		maximumMessageSizeInByte,
+		maximumMessageSizeInBytes,
 		"RAY_MALLOC_TYPE_OUTBOX_ALLOCATOR",false);
 
 	if(m_miniRanksAreEnabled){
@@ -1219,18 +1261,18 @@ void ComputeCore::configureEngine() {
 		#endif
 
 		m_bufferedInboxAllocator.constructor(m_maximumAllocatedOutboxBuffers,
-			maximumMessageSizeInByte,
+			maximumMessageSizeInBytes,
 			"RAY_MALLOC_TYPE_INBOX_ALLOCATOR/Buffered",false);
 
 		m_bufferedOutboxAllocator.constructor(m_maximumAllocatedOutboxBuffers,
-			maximumMessageSizeInByte,
+			maximumMessageSizeInBytes,
 			"RAY_MALLOC_TYPE_OUTBOX_ALLOCATOR/Buffered",false);
 
 	}
 
 	#ifdef CONFIG_DEBUG_CORE
-	cout<<"[ComputeCore] allocated "<<m_maximumAllocatedInboxBuffers<<" buffers of size "<<maximumMessageSizeInByte<<" for inbox messages"<<endl;
-	cout<<"[ComputeCore] allocated "<<m_maximumAllocatedOutboxBuffers<<" buffers of size "<<maximumMessageSizeInByte<<" for outbox messages"<<endl;
+	cout<<"[ComputeCore] allocated "<<m_maximumAllocatedInboxBuffers<<" buffers of size "<<maximumMessageSizeInBytes<<" for inbox messages"<<endl;
+	cout<<"[ComputeCore] allocated "<<m_maximumAllocatedOutboxBuffers<<" buffers of size "<<maximumMessageSizeInBytes<<" for outbox messages"<<endl;
 	#endif
 
 }